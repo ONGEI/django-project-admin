a1f6d9b9266428d7d6522fcdd334b36ab3b8f149
O.N.G.E.I <ongei.pcm.peru@gmail.com>
2012-03-29 12:01:54 -0500

    projectadmin init

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "Django-Project-Admin",
+    version = "0.1",
+    url = 'https://github.com/ONGEI/django-project-admin',
+    license = 'GPL V.3',
+    description = "Aplicación escrita en django que permite darle un seguimiento a un proyecto, nada que no sea conocido, asignación de tareas por ticket, wiki, archivos, etc.",
+    long_description = read('README.rst'),
+
+    author = 'O.N.G.E.I',
+    author_email = 'ONGEI@pcm.gob.pe',
+
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    
+    install_requires = ['setuptools'],
+
+    classifiers = [
+        'Development Status :: 4 - Dev',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPL License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
